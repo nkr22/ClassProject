b3daafb00d435a29e476e25d8e0ef7b5f2e9e48b
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-11-01 14:20:11 -0600

    set up django admin page and database

--- classproject/classproject/settings.py
+++ classproject/classproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'newapp.apps.NewappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'classproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'is_department_employment',
+        'USER': 'root',
+        'PASSWORD': 'N0elia0407!',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
