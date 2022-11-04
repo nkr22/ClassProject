b661ec8d903c0765b6a3395d704759503f5dd991
sierragriffin <91564329+sierragriffin@users.noreply.github.com>
2022-11-03 20:18:32 -0600

    Add files via upload
    BASIC: Added index page + bootstrap template

--- classproject/classproject/settings.py
+++ classproject/classproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'newapp.apps.NewappConfig',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'classproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,9 +81,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'is_department_employment',
         'USER': 'root',
-        'PASSWORD': 'N0elia0407!',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'PASSWORD': 'password_here',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -121,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'classproject/static')
+]           
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- classproject/newapp/views.py
+++ classproject/newapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+        
+def indexPageView(request) :
+    return render(request, 'apppages/index.html')    
