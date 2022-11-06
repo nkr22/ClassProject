1b87717a3c375aceaf94224fe5a90e67f9bc463c
sierragriffin <91564329+sierragriffin@users.noreply.github.com>
2022-11-06 10:08:54 -0700

    Styled the index page and created a navbar

---
+++ newapp/views.py
@@ -0,0 +1,8 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse  
+
+def indexPageView(request) :
+    return render(request, 'newAppPages/index.html')
+
